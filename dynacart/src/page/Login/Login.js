af6e33b212500ab2cf2c84f8c31217949fb03176
Muhammad Bilal <111046732+mbilal234@users.noreply.github.com>
2024-08-07 11:49:25 -0400

    Update Login.js

--- dynacart/src/page/Login/Login.js
+++ dynacart/src/page/Login/Login.js
@@ -33,7 +33,7 @@ const Login = () => {
     };
 
     const onOrcidLogin = () => {
-        window.location.href = 'http://localhost:4000/orcid/login'; // Redirect to your server's ORCID login endpoint
+        window.location.href = 'https://maps-backend-hqytucreb-muhammad-bilals-projects-bd7acfbb.vercel.app//orcid/login'; // Redirect to your server's ORCID login endpoint
     };
 
     const handleCustomTokenLogin = async (firebaseToken) => {
