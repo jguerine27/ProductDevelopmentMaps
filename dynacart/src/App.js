ae61589f3dca68dc388574a6d814799ef2213448
Muhammad Bilal <mbilal.bese21seecs@seecs.edu.pk>
2024-08-08 15:39:27 -0400

    Update App.js

--- dynacart/src/App.js
+++ dynacart/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import Signup from './page/Signup/Signup';
@@ -8,17 +8,31 @@ import RedirectIfAuthenticated from './page/RedirectIfAuthenticated';
 import RedirectIfORCIDAuth from './page/RedirectIfORCIDAuth';
 
 function App() {
+  const [isORCIDLogin, setIsORCIDLogin] = useState(false);
+
+  useEffect(() => {
+    // Check if the user logged in via ORCID
+    const orcidAuth = localStorage.getItem('orcidAuth');
+    if (orcidAuth) {
+      setIsORCIDLogin(true);
+    }
+  }, []);
+
   return (
     <Router>
       <div>
         <section>
           <Routes>
             <Route path="/home" element={
-              <RedirectIfORCIDAuth>
+              isORCIDLogin ? (
+                <RedirectIfORCIDAuth>
+                  <Home />
+                </RedirectIfORCIDAuth>
+              ) : (
                 <ProtectedRoute>
                   <Home />
                 </ProtectedRoute>
-              </RedirectIfORCIDAuth>
+              )
             } />
             <Route path="/signup" element={
               <RedirectIfAuthenticated>
